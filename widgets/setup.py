7cdcd149ce2a2f752f36bb10e3858ef71631a3ae
Alexandre Castro <contato@alexandrecastro.com>
2022-05-22 21:41:16 -0300

    feat: setup screen that tells the user about the camera calibration

---
+++ widgets/setup.py
@@ -0,0 +1,9 @@
+
+from kivymd.uix.screen import MDScreen
+from kivy.lang.builder import Builder
+
+Builder.load_file('./widgets/setup.kv')
+
+class Setup(MDScreen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
